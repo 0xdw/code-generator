5319c76d0449f3416af7648082d072b9404feb1e
Dinindu D. Wanniarachchi <dinindudiyanuwan@gmail.com>
2018-01-13 10:12:40 +0530

    Added Resources & error handling

--- generator.cc
+++ generator.cc
@@ -9,33 +9,39 @@ std::string toUpperCase(std::string str) {
 }
 
 int main(int argc, char* argv[]) {
+    try {
+        std::string name = argv[1];
+        std::string class_name = argv[2];
 
-    std::string name = argv[1];
-    std::string class_name = argv[2];
+        std::ofstream source_file;
+        std::ofstream header_code;
 
-    std::ofstream source_file;
-    std::ofstream header_code;
+        source_file.open (name + ".cc");
+        header_code.open (name + ".h");
 
-    source_file.open (name + ".cc");
-    header_code.open (name + ".h");
+        source_file << "#include \"" + name + ".h\"\n\n";
+        source_file << "" + class_name + "::" + class_name + "() { }\n";
+        source_file << "" + class_name + "::~" + class_name + "() { }\n";
 
-    source_file << "#include \"" + name + ".h\"\n\n";
-    source_file << "" + class_name + "::" + class_name + "() { }\n";
-    source_file << "" + class_name + "::~" + class_name + "() { }\n";
+        std::string header = toUpperCase(name) + "_H";
 
-    std::string header = toUpperCase(name) + "_H";
-
-    header_code << "#ifndef " + header + "\n";
-    header_code << "#define " + header + "\n\n";
-    header_code << "class " + class_name + " {\n";
-    header_code << "    public:\n";
-    header_code << "        " + class_name + "();\n";
-    header_code << "        ~" + class_name + "();\n";
-    header_code << "};\n\n";
-    header_code << "#endif // " + header + "\n";
-    
-    source_file.close();
-    header_code.close();
+        header_code << "#ifndef " + header + "\n";
+        header_code << "#define " + header + "\n\n";
+        header_code << "class " + class_name + " {\n";
+        header_code << "    public:\n";
+        header_code << "        " + class_name + "();\n";
+        header_code << "        ~" + class_name + "();\n";
+        header_code << "};\n\n";
+        header_code << "#endif // " + header + "\n";
+        
+        source_file.close();
+        header_code.close();
+    } catch(...) {
+        std::cout << "Invalid number of arguments." << std::endl;
+        std::cout << "To generate file." << std::endl;
+        std::cout << "$ generate <file_name> <class_name>\n" << std::endl;
+        system("pause");
+    }
 
     return 0;
 }
