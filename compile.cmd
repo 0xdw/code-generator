743e3ca5281ebcf126207d377f9f08fd86b0e603
Dinindu D. Wanniarachchi <dinindudiyanuwan@gmail.com>
2018-01-17 20:54:23 +0530

    Added generator types

--- compile.cmd
+++ compile.cmd
@@ -1,2 +1,2 @@
 cmd /c windres resources/resource.rc -O coff -o resources/resource.res
-cmd /c g++ -g -std=c++11 -o generate generator.cc resources/resource.res
+cmd /c g++ -g -std=c++11 -o generator generator.cc resources/resource.res
--- generator.cc
+++ generator.cc
@@ -10,32 +10,47 @@ std::string toUpperCase(std::string str) {
 
 int main(int argc, char* argv[]) {
     try {
-        std::string name = argv[1];
-        std::string class_name = argv[2];
+        std::string type = argv[1];
+        std::string name = argv[2];
+        std::string class_name = argv[3];
 
         std::ofstream source_file;
         std::ofstream header_code;
 
         source_file.open (name + ".cc");
-        header_code.open (name + ".h");
-
-        source_file << "#include \"" + name + ".h\"\n\n";
-        source_file << "" + class_name + "::" + class_name + "() { }\n";
-        source_file << "" + class_name + "::~" + class_name + "() { }\n";
-
-        std::string header = toUpperCase(name) + "_H";
-
-        header_code << "#ifndef " + header + "\n";
-        header_code << "#define " + header + "\n\n";
-        header_code << "class " + class_name + " {\n";
-        header_code << "    public:\n";
-        header_code << "        " + class_name + "();\n";
-        header_code << "        ~" + class_name + "();\n";
-        header_code << "};\n\n";
-        header_code << "#endif // " + header + "\n";
+        if (type == "init") {
+            source_file << "#include <iostream>\n\n";
+            source_file << "int main(int argc, char* argv[]) {\n";
+            source_file << "    std::cout << \"Hello world\" << std::endl;\n";
+            source_file << "    return 0;";
+            source_file << "}";
+            source_file.close();
+            return 0;
+        }
         
-        source_file.close();
-        header_code.close();
+        if (type == "generate") {
+            header_code.open (name + ".h");
+
+            source_file << "#include \"" + name + ".h\"\n\n";
+            source_file << "" + class_name + "::" + class_name + "() { }\n";
+            source_file << "" + class_name + "::~" + class_name + "() { }\n";
+
+            std::string header = toUpperCase(name) + "_H";
+
+            header_code << "#ifndef " + header + "\n";
+            header_code << "#define " + header + "\n\n";
+            header_code << "class " + class_name + " {\n";
+            header_code << "    public:\n";
+            header_code << "        " + class_name + "();\n";
+            header_code << "        ~" + class_name + "();\n";
+            header_code << "};\n\n";
+            header_code << "#endif // " + header + "\n";
+            
+            source_file.close();
+            header_code.close();
+            return 0;
+        }
+        
     } catch(...) {
         std::cout << "Invalid number of arguments." << std::endl;
         std::cout << "To generate file." << std::endl;
